3195ed6c7101e6636fbcde22085e2f04a4d6b415
Diego <dgonzalez-2019203@kinal.edu.gt>
2021-06-07 01:43:39 -0600

    se agrego

---
+++ PruebaSDK/app/src/main/java/com/example/pruebasdk/ui/login/LoginActivity.java
@@ -0,0 +1,122 @@
+package com.example.pruebasdk.ui.login;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.pruebasdk.R;
+import com.example.pruebasdk.ui.login.LoginViewModel;
+import com.example.pruebasdk.ui.login.LoginViewModelFactory;
+import com.example.pruebasdk.databinding.ActivityLoginBinding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LoginViewModel loginViewModel;
+    private ActivityLoginBinding binding;
+
+    EditText username;
+    EditText idBuscado;
+    Button btnGuardar;
+    Button btnBuscar;
+
+    RequestQueue requestQueue;
+
+    private static final String URL1 = "http://192.168.0.9/blinkid/save.php";
+
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_login);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        initUi();
+
+        btnGuardar.setOnClickListener(this);
+    }
+
+    private void initUi() {
+        username = findViewById(R.id.username);
+
+        btnGuardar = findViewById(R.id.btnGuardar);
+    }
+
+    /**Override**/
+    @Override
+    public void onClick(View v){
+        int id = v.getId();
+
+        if(id == R.id.btnGuardar){
+            String sexDesc = username.getText().toString().trim();
+
+            createUser(sexDesc);
+        }else if(id == R.id.btnBuscar){
+            Intent intent = new Intent(this, LoginActivity2.class);
+            intent.putExtra("id", idBuscado.getText().toString().trim());
+            startActivity(intent);
+
+        }
+    }
+
+    private void createUser(final String sexDesc) {
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                URL1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(LoginActivity.this, "Correct", Toast.LENGTH_SHORT).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("sexDesc",sexDesc);
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+}
